1fe796c2065492304f3d33368953b0f0da1aed19
Deka Abdi <dekahassan16@gmail.com>
2021-09-21 23:57:27 -0700

    Feature2
    Sorting a list of strings

--- A1.java
+++ A1.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+
 public class A1 {
 
 //This feature about going to sorting a list of Strings
+public static void main(String args[])   
+{  
+//defining an array of type string  
+String[] clrs = {"Blue", "Green", "Red", "Yellow", "White"};
+
+//sorts string array in alphabetical order or ascending order  
+Arrays.sort(clrs);  
+//prints the sorted string array in ascending order  
+System.out.println(Arrays.toString(clrs));  
+}  
 
 }
