0100286b602fcda179bc75e2ea1ee62edc0ff452
Deka Abdi <dekahassan16@gmail.com>
2021-09-21 23:48:02 -0700

    Add main method

--- A1.java
+++ A1.java
@@ -1,13 +1,17 @@
 import java.util.Arrays;
 
 public class A1 {
+
+    public static void main(String[] args) {
+
+
 //This feature sorts an array of numbers
 int [] age = {13, 7, 6, 45, 21, 9, 101, 102};
 
 // Applying sort() method over to above array
 // by passing the array as an argument
-Arrays.sort(age);
+ Arrays.sort(age);
 //printing the sorted array
 System.out.println(age);
-
+    }
 }
