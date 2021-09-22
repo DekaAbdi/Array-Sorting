6bfdb93d8fd588112c7064285681e94073332c35
Deka Abdi <dekahassan16@gmail.com>
2021-09-21 23:34:07 -0700

    Update A1.java

--- A1.java
+++ A1.java
@@ -1,3 +1,13 @@
+import java.util.Arrays;
+
 public class A1 {
 //This feature sorts an array of numbers
+int [] age = {13, 7, 6, 45, 21, 9, 101, 102};
+
+// Applying sort() method over to above array
+// by passing the array as an argument
+Arrays.sort(age);
+//printing the sorted array
+System.out.println(age);
+
 }
