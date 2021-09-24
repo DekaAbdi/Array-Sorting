68611d73c745ce691bf713d08e2de9dd3a20d0c8
Deka Abdi <dekahassan16@gmail.com>
2021-09-23 18:26:32 -0700

    Update A1.java

--- A1.java
+++ A1.java
@@ -1,12 +1,25 @@
+import java.util.Arrays;
 
-//This feature about going to sorting a list of Strings
-public static void stringSort(String clrs[])
-{  
+public class A1 {
+   public static void main(String[] args) {
+//This feature about going to sorting a list of Strings 
+ String [] clrs = {"Blue", "White", "Red", "Green", "Yellow","Black"};
 
 //sorts string array in alphabetical order or ascending order  
 Arrays.sort(clrs);  
 //prints the sorted string array in ascending order  
-System.out.println(Arrays.toString(clrs));  
-}  
+String [] sorted = clrs;  
+
+//Testing to see if this works
+String [] result = {"Black", "Blue", "Green", "Red", "White", "Yellow"};
+if (Arrays.equals(sorted, result))   
+    System.out.println("Passed!");   
+else  
+    System.out.println("Failed!");   
+    }   
+}
+
+
+
 
 
